ea24f24c1afa6cedd19bad633e9efb6b1f74a621
Mary Rose Tan <mrosetan@gmail.com>
2015-10-31 21:31:47 +0800

    migrations & laravelcollective html

---
+++ database/migrations/2015_10_31_114000_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('friend_userid');
+            $table->bigInteger('userid');
+            $table->timestamp('date_added');
+            $table->timestamp('date_accepted');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('friends');
+    }
+}
