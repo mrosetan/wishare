d1b16468a34bbb1e6daf0c6809a5b09f036b36f4
Mary Rose Tan <mrosetan@gmail.com>
2015-11-02 17:23:55 +0800

    added username and made email unique - migrations

--- database/migrations/2015_10_31_110436_create_wishare_users_table.php
+++ database/migrations/2015_10_31_110436_create_wishare_users_table.php
@@ -14,11 +14,12 @@ class CreateWishareUsersTable extends Migration
     {
         Schema::create('wishare_users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('email', 255);
+            $table->string('email', 255)->unique();
             $table->string('password', 255);
             $table->string('imageurl');
             $table->string('firstname', 255);
             $table->string('lastname', 255);
+            $table->string('username', 255);
             $table->string('city', 255);
             $table->date('birthdate');
             $table->string('facebook', 255);
