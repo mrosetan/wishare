6b84dff9d3c60a4fe1ccb913a76f96d6eb906d9f
Julienne Camille Racuya <hulyenhaha@gmail.com>
2015-11-07 00:27:21 +0800

    merge user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,16 +7,23 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\UserRequest;
+use App\User;
+use Auth;
+
 class UserController extends Controller
 {
+
   public function dashboard()
   {
     return view('userlayouts-master.user-master');
   }
+
   public function home()
   {
     return view('userlayouts.home');
   }
+
   public function profile()
   {
     return view('userlayouts.profile');
@@ -45,5 +52,25 @@ class UserController extends Controller
   public function otheruser()
   {
     return view('otheruser.otheruserprofile');
+  }
+
+  public function store(UserRequest $request)
+  {
+
+    $user = new User(array(
+      'lastname' => trim($request->lastname),
+      'firstname' => trim($request->firstname),
+      'username' => trim($request->username),
+      'email' => trim($request->email),
+      'privacy' => 0,
+      'type' => 1,
+      'status' => 1,
+      'password' => trim(bcrypt($request->get('password'))),
+    ));
+
+    $user->save();
+
+    return redirect('/user/home');
+    refs/remotes/origin/develop
   }
 }
