d94bd55e324d431f0985fe320756255d797428d6
Mary Rose Tan <mrosetan@gmail.com>
2015-11-08 01:30:42 +0800

    CRUD Default wishlist

--- resources/views/admin/viewDefaultWishlists.blade.php
+++ resources/views/admin/viewDefaultWishlists.blade.php
@@ -8,46 +8,47 @@
             <div class="panel-heading">
                 <h3 class="panel-title">Wishlists</h3>
             </div>
+
+            @if(session('status'))
+              <div class="alert alert-success">
+                  {{ session('status') }}
+              </div>
+            @endif
+
+            @if($defaultwishlists->isEmpty())
+              <div class="alert alert-info" role="alert">
+                No Users.
+              </div>
+            @else
+
+            @foreach($errors->all() as $error)
+                <p class="alert alert-danger"> {{ $error }}</p>
+            @endforeach
             <div class="panel-body">
                 <table class="table table-striped">
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>Wishlist</th>
+                            <th>Default Wishlist</th>
                             <th>Date Created</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
+                      @foreach($defaultwishlists as $dw)
                         <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>12-12-12</td>
+                            <td>{!! $dw->id !!}</td>
+                            <td>{!! $dw->title !!}</td>
+                            <td>{!! $dw->created_at !!}</td>
                             <td>
-                              <a href="#"><span class="glyphicon glyphicon-edit"></span></a>
-                              <a href="#"><span class="glyphicon glyphicon-trash"></span></a>
+                              <a href="{!! action('AdminController@editDefaultWishlist', $dw->id) !!}"><span class="glyphicon glyphicon-edit"></span></a>
+                              <a href="{!! action('AdminController@deleteDefaultWishlist', $dw->id) !!}"><span class="glyphicon glyphicon-trash"></span></a>
                             </td>
                         </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>12-12-12</td>
-                            <td>
-                              <a href="#"><span class="glyphicon glyphicon-edit"></span></a>
-                              <a href="#"><span class="glyphicon glyphicon-trash"></span></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Larry</td>
-                            <td>12-12-12</td>
-                            <td>
-                              <a href="#"><span class="glyphicon glyphicon-edit"></span></a>
-                              <a href="#"><span class="glyphicon glyphicon-trash"></span></a>
-                            </td>
-                        </tr>
+                      @endforeach
                     </tbody>
                 </table>
+              @endif
             </div>
         </div>
 
