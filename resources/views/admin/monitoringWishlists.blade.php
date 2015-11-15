8ac6522e073dd2a3a2789c8fccf893890b7baef8
Mary Rose Tan <mrosetan@gmail.com>
2015-11-15 19:54:42 +0800

    Fixed Monitor Wishlists

--- resources/views/admin/monitoringWishlists.blade.php
+++ resources/views/admin/monitoringWishlists.blade.php
@@ -1,66 +1,7 @@
 @extends('admin.master')
 @section('title', 'Monitor Wishlists')
 @section('content')
-  <!-- <div class="row">
-      <div class="col-md-12">
 
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Wishlists</h3>
-            </div>
-
-            @if($wishlists->isEmpty())
-              <div class="alert alert-info" role="alert">
-                No Wishlists.
-              </div>
-            @else
-
-            @if(session('status'))
-              <div class="alert alert-success">
-                  {{ session('status') }}
-              </div>
-            @endif
-
-            <div class="panel-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Wishlist</th>
-                            <th>Created by</th>
-                            <th>Date Created</th>
-                            <th>Privacy Setting</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($wishlists as $w)
-                          <tr>
-                              <td>{!! $w->id !!}</td>
-                              <td>{!! $w->title !!}</td>
-                              <td>
-                                Name: {!! $w->user->firstname !!} {!! $w->user->lastname !!}<br />
-                                Username: {!! $w->user->username !!}<br />
-                                Email: {!! $w->user->email !!}<br />
-                              </td>
-                              <td>{!! $w->created_at !!}</td>
-                              <td>
-                                @if($w->privacy == 0)
-                                  Public
-                                @else
-                                  Private
-                                @endif
-                              </td>
-                          </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {!! $wishlists->render() !!}
-              @endif
-            </div>
-        </div>
-
-      </div>
-  </div> -->
 
 
   <div class="row">
@@ -88,8 +29,9 @@
                   <table class="table datatable">
                       <thead>
                           <tr>
-                            <th>#</th>
-                            <th>Wishlist</th>
+                            <th>Status</th>
+                            <th>Wishlist #</th>
+                            <th>Wishlist Title</th>
                             <th>Created by</th>
                             <th>Date Created</th>
                             <th>Privacy Setting</th>
@@ -98,12 +40,20 @@
                       <tbody>
                         @foreach($wishlists as $w)
                           <tr>
+                              <td>
+                                @if($w->status == 1)
+                                  <span class="fa fa-circle active-indicator"></span> Active
+                                @else
+                                  <span class="fa fa-circle deactivated-indicator"></span> Deleted
+                                @endif
+                              </td>
                               <td>{!! $w->id !!}</td>
                               <td>{!! $w->title !!}</td>
                               <td>
-                                Name: {!! $w->user->firstname !!} {!! $w->user->lastname !!}<br />
-                                Username: {!! $w->user->username !!}<br />
-                                Email: {!! $w->user->email !!}<br />
+                                User ID: {!! $w['user']['id'] !!}<br />
+                                Name: {!! $w['user']['firstname'] !!} {!! $w['user']['lastname'] !!}<br />
+                                Username: {!! $w['user']['username'] !!}<br />
+                                Email: {!! $w['user']['email'] !!}<br />
                               </td>
                               <td>{!! $w->created_at !!}</td>
                               <td>
