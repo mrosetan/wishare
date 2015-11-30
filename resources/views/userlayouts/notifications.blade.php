3fec82b74151bcf3eee42fe65f6d8394e739efee
Mary Rose Tan <mrosetan@gmail.com>
2015-12-01 02:22:12 +0800

    notifications

--- resources/views/userlayouts/notifications.blade.php
+++ resources/views/userlayouts/notifications.blade.php
@@ -25,67 +25,29 @@
             <!--notifications-->
             <div class="panel-body tab-content">
               <div class="tab-pane active" id="tab-notif">
-                @foreach($tags as $t)
-                  <div class="panel panel-default">
-                      <div class="panel-body">
-                        <div class="pull-left">
-                          {!! Html::image('' . $t->tagger->imageurl, '', array('class'=>'user-friend img-circle')) !!}
-                        </div>
-                        <div class="user-details">
-                          <p class="user-name">
-                            <a href="{!! action('UserController@otheruser', $t->tagger->id) !!}"> {!! $t->tagger->username !!} </a> tagged you in a <a href="{!! action('UserController@wish', $t->wish->id) !!}">wish</a>
-                            <p>
-                              {!! $t->created_at !!}
+                @if(count($tags)>0)
+                  @foreach($tags as $t)
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                          <div class="pull-left">
+                            {!! Html::image('' . $t->tagger->imageurl, '', array('class'=>'user-friend img-circle')) !!}
+                          </div>
+                          <div class="user-details">
+                            <p class="user-name">
+                              <a href="{!! action('UserController@otheruser', $t->tagger->id) !!}"> {!! $t->tagger->username !!} </a> tagged you in a <a href="{!! action('UserController@wish', $t->wish->id) !!}">wish</a>
+                              <p>
+                                {!! $t->created_at !!}
+                              </p>
                             </p>
-                          </p>
 
+                          </div>
                         </div>
-                      </div>
-                  </div>
-                @endforeach
-              </div>
-
-              <div class="tab-pane active" id="tab-gr">
-                  <div class="panel panel-default">
-                      <div class="panel-body">
-                        <div class="pull-left">
-                          {!! Html::image('' . $t->tagger->imageurl, '', array('class'=>'user-friend img-circle')) !!}
-                        </div>
-                        <div class="user-details">
-                          <p class="user-name">
-                            <p>Bobby granted your wish: Bobby</p>
-                            {!! Form::button('Accept', ['class'=>'btn btn-info', 'data-toggle'=>'modal', 'data-target'=>'#modal_acceptgrant'])!!}
-                            {!! Form::reset('Cancel', ['class'=>'btn btn-default'])!!}
-                            {!! Form::close() !!}
-                          </p>
-                        </div>
-                      </div>
-                  </div>
+                    </div>
+                  @endforeach
+                @endif
               </div>
 
 
-              <div class="modal" id="modal_acceptgrant" tabindex="-1" role="dialog" aria-labelledby="defModalHead" aria-hidden="true">
-                  <div class="modal-dialog">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                              <h4 class="modal-title" id="defModalHead">Accept Grant Request</h4>
-                          </div>
-                          <div class="modal-body">
-                              <h5>Bobby granted your wish: Bobby</h5> <br />
-                              {!! Form::open() !!}
-                              {!! Form::text('caption', null, array('class'=>'form-control', 'placeholder'=>'Add a caption')) !!}
-                              <br />
-                              {!! Form::file('photo')!!}
-                          </div>
-                          <div class="modal-footer">
-                            {!! Form::button('Accept', ['class'=>'btn btn-info', 'data-toggle'=>'modal', 'data-target'=>'#modal_acceptgrant'])!!}
-                            {!! Form::reset('Cancel', ['class'=>'btn btn-default'])!!}
-                            {!! Form::close() !!}
-                          </div>
-                      </div>
-                  </div>
-              </div>
 
               <!--================================friend requests================================-->
 
@@ -133,6 +95,51 @@
               </div>
 
               <!--================================friend requests================================-->
+
+
+              <div class="tab-pane" id="tab-gr">
+                  <div class="panel panel-default">
+                      <div class="panel-body">
+                        <div class="pull-left">
+                          IMAGE
+                        </div>
+                        <div class="user-details">
+                          <p class="user-name">
+                            <p>Bobby granted your wish: Bobby</p>
+                            {!! Form::button('Accept', ['class'=>'btn btn-info', 'data-toggle'=>'modal', 'data-target'=>'#modal_acceptgrant'])!!}
+                            {!! Form::reset('Cancel', ['class'=>'btn btn-default'])!!}
+                            {!! Form::close() !!}
+                          </p>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+
+
+              <div class="modal" id="modal_acceptgrant" tabindex="-1" role="dialog" aria-labelledby="defModalHead" aria-hidden="true">
+                  <div class="modal-dialog">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                              <h4 class="modal-title" id="defModalHead">Accept Grant Request</h4>
+                          </div>
+                          <div class="modal-body">
+                              <h5>Bobby granted your wish: Bobby</h5> <br />
+                              {!! Form::open() !!}
+                              {!! Form::text('caption', null, array('class'=>'form-control', 'placeholder'=>'Add a caption')) !!}
+                              <br />
+                              {!! Form::file('photo')!!}
+                          </div>
+                          <div class="modal-footer">
+                            {!! Form::button('Accept', ['class'=>'btn btn-info', 'data-toggle'=>'modal', 'data-target'=>'#modal_acceptgrant'])!!}
+                            {!! Form::reset('Cancel', ['class'=>'btn btn-default'])!!}
+                            {!! Form::close() !!}
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+
             </div>
         </div>
         <!---->
