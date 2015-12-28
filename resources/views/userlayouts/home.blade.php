8c55818e0afd641bf7a1fdcd163a261af92f7368
Mary Rose Tan <mrosetan@gmail.com>
2015-12-29 03:54:22 +0800

    event stream updates

--- resources/views/userlayouts/home.blade.php
+++ resources/views/userlayouts/home.blade.php
@@ -115,6 +115,23 @@
                         &nbsp;&nbsp;
                         <span data-toggle="tooltip" data-placement="top" title="Grant"><a data-toggle="modal" data-target="#modal_grant{!! $s['wishid'] !!}"><span class="fa fa-magic"></span></a></span>
                         @endif
+
+                        @if($s['userid'] == $user->id)
+                          &nbsp;&nbsp;
+                          <a href="#" data-toggle="modal" data-target="#modalwish{!! $s['wishid'] !!}">
+                            <span class="fa fa-edit"></span>
+                          </a>
+
+                          &nbsp;&nbsp;
+                          <a data-toggle="tooltip" data-placement="top" title="Tag" href="{!! url('user/edit/tags', $s['wishid']) !!}">
+                            <span class="fa fa-tag"></span>
+                          </a>
+
+                          &nbsp;&nbsp;
+                          <a data-toggle="tooltip" data-placement="top" title="delete" href="#" class="mb-control" data-box="#mb-deletewish{!! $s['wishid'] !!}">
+                            <span class="fa fa-trash-o"></span>
+                          </a>
+                        @endif
                       </div>
                     </div>
 
@@ -330,6 +347,22 @@
                         &nbsp;&nbsp;
                         <a href="{!! action('UserController@rewishDetails', $s['wishid']) !!}" data-toggle="tooltip" data-placement="top" title="Rewish"><span class="fa fa-retweet"></span></a>
 
+                        @if($s['userid'] == $user->id)
+                          &nbsp;&nbsp;
+                          <a href="#" data-toggle="modal" data-target="#modalwish{!! $s['wishid'] !!}">
+                            <span class="fa fa-edit"></span>
+                          </a>
+
+                          &nbsp;&nbsp;
+                          <a data-toggle="tooltip" data-placement="top" title="Tag" href="{!! url('user/edit/tags', $s['wishid']) !!}">
+                            <span class="fa fa-tag"></span>
+                          </a>
+
+                          &nbsp;&nbsp;
+                          <a href="#" data-toggle="tooltip" data-placement="top" title="Delete" class="mb-control" data-box="#mb-deletewish{!! $s['wishid'] !!}">
+                            <span class="fa fa-trash-o"></span>
+                          </a>
+                        @endif
                       </div>
                     </div>
 
@@ -519,5 +552,124 @@
   </div>
   <!-- END BLUEIMP GALLERY -->
   </div>
+
+
+  <!-- =================================== WISHES =================================== -->
+
+
+    @if(count($fstream) > 0)
+      @foreach($fstream as $s)
+        @if($s['userid'] == $user->id)
+            <!-- message box-->
+            <div class="message-box animated fadeIn" data-sound="alert" id="mb-deletewish{!! $s['wishid'] !!}">
+                <div class="mb-container">
+                    <div class="mb-middle">
+                        <div class="mb-title"><span class="glyphicon glyphicon-trash"></span>Delete Wish</div>
+                        <div class="mb-content">
+                            <p>Are you sure you want to delete this wish?</p>
+                        </div>
+                        <div class="mb-footer">
+                            <div class="pull-right">
+                                <a href="{!! action('UserController@deleteWish', $s['wishid']) !!}" class="btn btn-success btn-lg">Yes</a>
+                                <button class="btn btn-default btn-lg mb-control-close">No</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- message box-->
+
+            <!-- modal -->
+
+            <div class="modal" id="modalwish{!! $s['wishid'] !!}" tabindex="-1" role="dialog" aria-labelledby="defModalHead" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                      <h4>Edit Wish</h4>
+                    </div>
+                    <div class="modal-body">
+                      @if(session('wishStatus'))
+                        <div class="alert alert-success">
+                            {{ session('wishStatus') }}
+                        </div>
+                      @endif
+                      @foreach($errors->all() as $error)
+                          <p class="alert alert-danger"> {{ $error }}</p>
+                      @endforeach
+                      {!! Form::open(array( 'action' => array('UserController@updateWish', $s['wishid']),
+                                            'class' => 'form',
+                                            'files'=>true,
+                                            'enctype'=>'multipart/form-data')) !!}
+                      <div class="form-group">
+                        <div class="row">
+                          <div class="col-md-12">
+                            {!! Form::select('wishlist', $wishlists, $s['wishlistid'], array('class'=>'form-control'))!!}
+                          </div>
+                        </div>
+                        <br />
+                        <div class="row">
+                          <div class="col-md-12">
+                            {!! Form::text('title', $s['title'], array('class'=>'form-control', 'placeholder'=>'Wish')) !!}
+                          </div>
+                        </div>
+                        <br />
+                        <div class="row">
+                          <div class="col-md-12">
+                            {!! Form::textarea('details', $s['details'], ['class'=>'form-control ', 'placeholder'=>'Details or specifics about the wish', 'size'=>'102x5']) !!}
+                          </div>
+                        </div>
+                        <br />
+                        <div class="row">
+                          <div class="col-md-12">
+                            {!! Form::textarea('alternatives', $s['alternatives'], ['class'=>'form-control ', 'placeholder'=>'Wish alternatives', 'size'=>'102x5']) !!}
+                          </div>
+                        </div>
+                        <br />
+                        <div class="row">
+                          <div class="col-sm-12">
+                            <label>Due Date</label>
+                            {!! Form::text('due_date', $s['due_date'], array('id'=>'datepicker', 'class'=>'form-control')) !!}
+                          </div>
+                        </div>
+                        <br />
+                        <div class="row">
+                          <div class="col-sm-12">
+                            {!! Form::file('wishimageurl', array('class'=>'fileinput btn btn-info')) !!}
+                          </div>
+                        </div>
+                        <br />
+                        <div class="row">
+                          <div class="col-md-12">
+                            @if($s['flagged'] ==  1)
+                              {!! Form::checkbox('flag', '1', true) !!}
+                            @else
+                              {!! Form::checkbox('flag', '1') !!}
+                            @endif
+                            <label><span class="glyphicon glyphicon-flag"></span> Flag </label>
+                            <!-- <span class="glyphicon glyphicon-flag"></span><a href="#"><span class="xn-text">&nbsp;Flag wish</span></a> -->
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="pull-right">
+                              {!! Form::submit('Update', array('class'=>'btn btn-info')) !!}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      {!! Form::close() !!}
+                    </div>
+                </div>
+              </div>
+            </div>
+          <!-- modal -->
+
+          @endif
+
+
+      @endforeach
+    @endif
+
 </div>
 @endsection
