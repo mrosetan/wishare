7b902ef0e62fc917eef6b95c0df13a8fc6b846e8
Mary Rose Tan <mrosetan@gmail.com>
2016-02-04 22:33:41 +0800

    meta

--- resources/views/userlayouts-master/profile-master.blade.php
+++ resources/views/userlayouts-master/profile-master.blade.php
@@ -3,7 +3,7 @@
     <head>
         <!-- META SECTION -->
         <title>@yield('title')</title>
-        <<meta http-equiv="Content-Type"     content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type"     content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible"  content="IE=edge" />
         <meta name="viewport"               content="width=device-width, initial-scale=1" />
         <meta name="csrf-token"             content="{{ csrf_token() }}">
